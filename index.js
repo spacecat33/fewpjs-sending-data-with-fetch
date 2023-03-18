7a2fe819626484db14bb094b3052c54fd329ea5c
Janan Akkad <jananakkad@hotmail.co.uk>
2023-03-18 14:27:48 +0000

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Add your code here
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+
+
+
+//   OR:
+
+
+//   const configObj = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(submitData)
+//   };
+  
+//   function submitData(name, email){
+//     return fetch("http://localhost:3000/users", configObj)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
+// } 
